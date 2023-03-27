d24a651e7c49003537a05677c1c0e81e10ab176b
DiegoRCA <diegocartelle@gmail.com>
2023-03-27 14:12:45 +1100

    14

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, '/public'))
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+    res.sendFile('index.html');
 })
 
 const port = process.env.PORT || 3000
