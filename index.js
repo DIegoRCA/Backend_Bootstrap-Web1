abde2085497f197507cf1520a02c06f8c43b23b7
DiegoRCA <diegocartelle@gmail.com>
2023-03-27 13:23:45 +1100

    5

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express')
+
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
-  });
+    res.send('Express JS on Vercel')
+})
 
-app.use(express.static('public'));
+app.get('/ping', (req, res) => {
+    res.send('pong 🏓')
+})
 
-app.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 8080
 
-
+app.listen(port, (err, res) => {
+    if (err) {
+        console.log(err)
+        return res.status(500).send(err.message)
+    } else {
+        console.log('[INFO] Server Running on port:', port)
+    }
+})
 
