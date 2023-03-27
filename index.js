9fa96929f38edd80c8df169dbf42f21b38289c81
DiegoRCA <diegocartelle@gmail.com>
2023-03-27 13:20:15 +1100

    4

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-import express, { static } from 'express';
-const app = express();
+const express = require('express')
+const app = express()
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
   });
 
-app.use(static('public'));
+app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000);
 
-export default app
 
 
