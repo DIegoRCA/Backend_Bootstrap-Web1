89f24dcb204cafcb705d93bd476c278a04e59c1f
DiegoRCA <diegocartelle@gmail.com>
2023-03-27 15:36:22 +1100

    30

--- index.js
+++ index.js
@@ -5,18 +5,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.js'));
+    res.sendFile(path.join(__dirname, 'public','/index.html'));
 });
 
 
 
-app.listen(port, (err, res) => {
-    if (err) {
-        console.log(err)
-        return res.status(500).send(err.message)
-    } else {
-        console.log('[INFO] Server Running on port:', port)
-    }
-})
+app.listen(port);
 
 module.exports = app
